61a3efa691924908f3699c992ca974763a8c9928
Alex Speaks <login@sneaksneak.org>
2014-09-03 08:56:39 -0700

    A little cleanup and successful test of mkAD()

--- newecouser.py
+++ newecouser.py
@@ -26,27 +26,24 @@ from pyad import *
 #from urllib import urlencode
 
 
+
 def mkAD(username):
 	ou = pyad.adcontainer.ADContainer.from_dn("OU=folder redirection,OU=employees,OU=ecotrust,DC=ecotrust,DC=org")
 	c = pyad.aduser.ADUser.create(name = username, container_object=ou, password=pwgen(), upn_suffix=None, enable=True, optional_attributes=dict(description = "new user"))
 	return c.displayName
-	'''	pyad.from_dn("CN=Ryan Genuson,OU=point97,DC=ecotrust,DC=org")	
-	newuser=pyad.aduser.ADUser('aspeaks')
-	#date=newuser.get_password_last_set()
-	#return date
-'''
 
 def pwgen():
 	return("Password!")
 
-print mkAD("aaron test")
-
-#def mkBasecamp()
-
 """
-def mkSocialcast()
+def mkBasecamp()
+
+
+def mkSocialcast():
 	httpCall = Http()
 	data = dict(name="Joe", comment="A test comment")
 	resp, content = h.request("http://bitworking.org/news/223/Meet-Ares", "POST", urlencode(data))
 	resp
 """
+
+print mkAD("aaron")
