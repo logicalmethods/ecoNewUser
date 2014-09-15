4207fa5926982438c543a27077c9f185204cdbff
Alex Speaks <alex@sneaksneak.org>
2014-09-14 19:13:37 -0700

    test: groups from config file

--- newecouser.py
+++ newecouser.py
@@ -13,10 +13,12 @@ def readWorkFile(fileName): #opens the to-do file and returns a json object of t
 def mkAD(userData): #create an ecotrust standard AD user
 	ou = pyad.adcontainer.ADContainer.from_dn("OU=employees,OU=ecotrust,DC=ecotrust,DC=org")
 	c = pyad.aduser.ADUser.create(name = userData["firstName"]+" "+userData["lastName"], container_object=ou, password=pwgen(), upn_suffix=None, enable=True, optional_attributes=dict(description = userData["description"], givenName = userData["firstName"], sn=userData["lastName"],displayName=userData["firstName"]+" "+userData["lastName"],sAMAccountName=userData["userName"],userPrincipalName=userData["userName"]+"@ecotrust.org", homeDirectory="\\\\daryl\\users\\"+userData["userName"],homeDrive="u:"))
-	addToGrp("everybody", userData["firstName"]+" "+userData["lastName"])
-	addToGrp("development_ro", userData["firstName"]+" "+userData["lastName"])
-	addToGrp("development_ro", userData["firstName"]+" "+userData["lastName"])
-	addToGrp("terra access", userData["firstName"]+" "+userData["lastName"])
+	#addToGrp("everybody", userData["firstName"]+" "+userData["lastName"])
+	#addToGrp("development_ro", userData["firstName"]+" "+userData["lastName"])
+	#addToGrp("development_ro", userData["firstName"]+" "+userData["lastName"])
+	#addToGrp("terra access", userData["firstName"]+" "+userData["lastName"])
+	for i in userData[groups]
+		addToGrp(i,userData["firstName"]+" "+userData["lastName"])
 	return(c.displayName)
 
 
